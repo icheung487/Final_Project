62e97e806b95bb753c8e1bfdedd59548baefefe5
karissapadilla <91580326+karissapadilla@users.noreply.github.com>
2022-03-12 23:58:34 -0500

    Add files via upload
    updated final_project and uploaded top_10 js file

--- Interactive_Map/static/js/final_project.js
+++ Interactive_Map/static/js/final_project.js
@@ -38,28 +38,45 @@ let baseMaps = {
   "Dark": dark
 };
 
-
 // FINAL PROJECT: UPDATE NAMING CONVENTION TO: CURRENT COVID CASES, PROJECTED COVID CASES, AND % TOURIST ARRIVAL
-let currentCovidCases = new L.LayerGroup();
 let projectedCovidCases = new L.LayerGroup();
+let currentCovidCases = new L.LayerGroup();
 let vaccinated = new L.LayerGroup();
 
 // // FINAL PROJECT: UPDATE NAMING CONVENTION TO: CURRENT COVID CASES, PROJECTED COVID CASES, AND % TOURIST ARRIVAL
 let overlays = {
+  "Top 10 Safest Cities": projectedCovidCases,
   "Current Covid Cases": currentCovidCases,
-  "Projected Covid Cases": projectedCovidCases,
   "Total Vaccinated": vaccinated,
 };
 
 // Then we add a control to the map that will allow the user to change which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
+// Get data from top_10s
+let top10 = top_10;
+
+// Projected Covid Cases
+top10.forEach(function(top_10) {
+L.marker(top_10.coordinates, {
+  radius: top_10.predicted_values,
+  color: "#DDCAE2",
+  opacity: 1,
+  fillOpacity: 0.5,
+  stroke: true,
+  weight: 0.5
+  })
+  .bindPopup("<h2>" + top_10.province_state + ", " + top_10.country_region + "</h2> <hr> <h3>Projected Covid Cases: " + top_10.predicted_values + "</h3>")
+
+.addTo(projectedCovidCases);
+projectedCovidCases.addTo(map);})
+
 // Get data from cities.js
 let cityData = cities;
 
 // Circle Marker for cityData
 cityData.forEach(function(cities) {
-  console.log(cities)
+  // console.log(cities)
 L.circleMarker(cities.coordinates, {
     radius: cities.confirmed_cases/10000000,
     color: "#B50027",
@@ -68,7 +85,7 @@ L.circleMarker(cities.coordinates, {
     stroke: true,
     weight: 0.5
   })
-  .bindPopup("<h2>" + cities.provine_state + ", " + cities.country_region + "</h2> <hr> <h3>Confirmed Cases: " + cities.confirmed_cases.toLocaleString() + "</h3>")
+  .bindPopup("<h2>" + cities.province_state + ", " + cities.country_region + "</h2> <hr> <h3>Confirmed Cases: " + cities.confirmed_cases.toLocaleString() + "</h3>")
   .addTo(currentCovidCases);
   currentCovidCases.addTo(map);
 
@@ -80,55 +97,7 @@ L.circleMarker(cities.coordinates, {
   stroke: true,
   weight: 0.5
   })
-  .bindPopup("<h2>" + cities.provine_state + ", " + cities.country_region + "</h2><hr><h3>Total Vaccinated: " + cities.people_fully_vaccinated.toLocaleString() + "</h3>")
+  .bindPopup("<h2>" + cities.province_state + ", " + cities.country_region + "</h2><hr><h3>Total Vaccinated: " + cities.people_fully_vaccinated.toLocaleString() + "</h3>")
 .addTo(vaccinated);
 vaccinated.addTo(map);
 });
-
-// Catch Error
-try {
-  nonExistentFunction();
-} catch (error) {
-  console.error(error);}
-
-// Projected Covid Cases
-// L.circleMarker(cities.Coordinates, {
-//   radius: cities.projected_covid_cases/1000000,
-//   color: "#DDCAE2",
-//   opacity: 1,
-//   fillOpacity: 0.5,
-//   stroke: true,
-//   weight: 0.5
-//   })
-//   .bindPopup("<h2>" + cities.Provine_State + ", " + cities.Country_Region + "</h2> <hr> <h3>"Projected Covid Cases " + cities.projected_covid_cases.toLocaleString() + "</h3>")
-
-// // }).addTo(allEarthquakes);
-// .addTo(projectedCovidCases);
-// // FINAL PROJECT: ADD TO CONFIRMED COVID CASES MAP
-// // Then we add the earthquake layer to our map.
-// projectedCovidCases.addTo(map);
-// });
-
-
-// Here we create a legend control object.
-// let legend = L.control({
-//   position: "bottomright"
-// });
-
-// legend.onAdd = function() {
-//   let div = L.DomUtil.create('div', 'info legend');
-
-//     labels = ['<strong>Layers</strong>'],
-//     categories = ['Confirmed Covid Cases','Total Vaccinated','Projected Covid Cases'];
-    
-//     const colors = ["#FF0000","#008000"];
-
-//     for (var i = 0; i < categories.length; i++) {
-//       console.log(colors[i]);
-//       div.innerHTML += 
-//         '<i class="circle" style="background:' + colors[i] + '"></i> ' +
-//             categories[i] + categories[i + 1] ? "&dash;" + categories[i + 1] + "<br>" : '+';
-//         }
-//         return div;
-//     };
-//     legend.addTo(map);
