8c99e258cf76af08a0861470b3d379d414e97ae2
icheung487 <girlwinks@gmail.com>
2022-03-23 20:03:47 -0400

    files

---
+++ Vacation Project/static/js/final_project.js
@@ -0,0 +1,111 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+// We create the second tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
+
+
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// MAYBE ADD BOUNDARY? 
+// Create the map object with a center and zoom level.
+let map = L.map("Map", {
+  center: [40.85209301,
+    -73.86282755],
+  zoom: 3,
+  layers: [streets]
+});
+
+// Create a base layer that holds all three maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets,
+  "Dark": dark
+};
+
+// FINAL PROJECT: UPDATE NAMING CONVENTION TO: CURRENT COVID CASES, PROJECTED COVID CASES, AND % TOURIST ARRIVAL
+let projectedCovidCases = new L.LayerGroup();
+let currentCovidCases = new L.LayerGroup();
+let vaccinated = new L.LayerGroup();
+
+// // FINAL PROJECT: UPDATE NAMING CONVENTION TO: CURRENT COVID CASES, PROJECTED COVID CASES, AND % TOURIST ARRIVAL
+let overlays = {
+  "Top 10 Safest Cities": projectedCovidCases,
+  "Current Covid Cases": currentCovidCases,
+  "Total Vaccinated": vaccinated,
+};
+var mapTab = document.getElementById('Map');
+  var observer1 = new MutationObserver(function(){
+    if(mapTab.style.display != 'none'){
+      map.invalidateSize();
+    }
+  });
+  observer1.observe(mapTab, {attributes: true}); 
+// Then we add a control to the map that will allow the user to change which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// Get data from top_10s
+let top10 = top_10;
+
+// Projected Covid Cases
+top10.forEach(function(top_10) {
+L.marker(top_10.coordinates, {
+  radius: top_10.predicted_values,
+  color: "#DDCAE2",
+  opacity: 1,
+  fillOpacity: 0.5,
+  stroke: true,
+  weight: 0.5
+  })
+  .bindPopup("<h2>" + top_10.province_state + ", " + top_10.country_region + "</h2> <hr> <h3>Projected Covid Cases: " + top_10.predicted_values + "</h3>")
+
+.addTo(projectedCovidCases);
+projectedCovidCases.addTo(map);})
+
+// Get data from cities.js
+let cityData = cities;
+
+// Circle Marker for cityData
+cityData.forEach(function(cities) {
+  // console.log(cities)
+L.circleMarker(cities.coordinates, {
+    radius: cities.confirmed_cases/10000000,
+    color: "#B50027",
+    opacity: 1,
+    fillOpacity: 0.5,
+    stroke: true,
+    weight: 0.5
+  })
+  .bindPopup("<h2>" + cities.provine_state + ", " + cities.country_region + "</h2> <hr> <h3>Confirmed Cases: " + cities.confirmed_cases.toLocaleString() + "</h3> <hr> <h4> Last Update: " + cities.last_update + "</h4>")
+  .addTo(currentCovidCases);
+  currentCovidCases.addTo(map);
+
+L.circleMarker(cities.coordinates, {
+  radius: cities.people_fully_vaccinated/10000000,
+  color: "#388D5D",
+  opacity: 1,
+  fillOpacity: 0.5,
+  stroke: true,
+  weight: 0.5
+  })
+  .bindPopup("<h2>" + cities.provine_state + ", " + cities.country_region + "</h2><hr><h3>Total Vaccinated: " + cities.people_fully_vaccinated.toLocaleString() + "</h3> <hr> <h4> Last Update: " + cities.last_update + "</h4>")
+.addTo(vaccinated);
+vaccinated.addTo(map);
+});
---
+++ Vacation Project/static/js/plots.js
@@ -0,0 +1,142 @@
+
+
+var dataArr = Plotly.d3.json("../static/data/tree_month_history.json", function(err, data){
+    function unpack(data, key) {
+        return data.map(function(item) { return item[key]; });
+    }
+   
+    var state_province = unpack(data, 'province_state')
+    var cases = unpack(data, 'confirmed_cases')
+    var vaccinated = unpack(data, 'people_fully_vaccinated')
+    var  MonthHistory = unpack(data, 'month')
+    
+    // Vaccination Plot
+    var trace = {
+        x: state_province,
+        y: vaccinated,
+        type: "bar"
+        };
+    // Create the data array for the plot
+    var VaccinBarr = [trace];
+    // Define the plot layout
+    var layout = {
+        title: "Vaacination",
+        xaxis: { title: "State Province" },
+        yaxis: { title: "People fully vaccinated" }
+        };
+    //console.log(VaccinBarr)
+     Plotly.react('plot0', VaccinBarr, layout);
+
+
+    //Cases plot    
+    var trace1 = {
+        x: state_province,
+        y: cases,
+        type: "bar"
+        };
+    // Create the data array for the plot
+    var CasesBarr = [trace1];
+      
+    // Define the plot layout
+    var layout1 = {
+        title: "Cases",
+        xaxis: { title: "Sate Province" },
+        yaxis: { title: "Cases" }
+      };
+    Plotly.react('plot1', CasesBarr, layout1);
+    
+    // line chart 
+    var trace2 = {
+        x: state_province,
+        y: cases,
+        mode: 'lines',
+        connectgaps: true
+      };
+      
+      var CasesDate = [trace2];
+      
+      var layout2 = {
+        title: 'Cases history for the past 3 Months',
+        showlegend: false
+      };
+      
+       Plotly.react('plot2', CasesDate, layout2);  
+ 
+    var values = state_province;
+ 
+    var select = d3.select("#selDataset");
+
+    list = values.filter(function (x, i, a) { 
+        return a.indexOf(x) === i; 
+    });
+
+    for (val of list)
+    {
+        select.append("option").text(val).property("value",val);
+    }
+ 
+});
+
+function optionChanged(place){
+    Plotly.d3.json("../static/data/tree_month_history.json", function(err, data){
+        function unpack(data, key) {
+            return data.map(function(item) { return item[key]; });
+        }
+        // filter based on place 
+        const result = top_10.filter(({province_state}) => province_state === place);
+
+        // ploting filtred data 
+        result.forEach(function (item) {
+            console.log(item)
+            const config = {
+                data: {
+                    datasets: [{
+                        type: 'line',
+                        label: 'Cases',
+                        data: [{
+                            x: item[1],
+                            y: item[8]
+                        }],
+                        fill: false,
+                        pointRadius: 0,
+                        tension: 0.1,
+                        borderColor: 'rgb(0, 128, 0)',
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            };
+            let ctx = document.getElementById('plot2');
+            let myChart = new Chart(ctx, config);
+        })
+
+    });
+
+    console.log(place);
+}
+
+function openTab(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+  
+      // Remove the background color of all tablinks/buttons
+      tablinks = document.getElementsByClassName("tablink");
+      for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].style.backgroundColor = "";
+        }
+  
+      // Show the specific tab content
+       document.getElementById(pageName).style.display = "block";
+      // Add the specific color to the button used to open the tab content
+       elmnt.style.backgroundColor = color;
+      
+    }  
